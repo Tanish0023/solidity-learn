b1cf4291483ca3ae4bfc5f0ba724e640d8cfde74
Tanish0023 <tanish23aggarwal@gmail.com>
2025-01-18 20:38:34 +0530

    testing1

--- contracts/sum.sol
+++ contracts/sum.sol
@@ -3,5 +3,17 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Sum{
+    uint value;
     
+    constructor(uint _initialValue){
+        value = _initialValue;
+    }
+
+    function add(uint _addValue) public  {
+        value += _addValue;
+    }
+
+    function getValue() public view returns (uint) {
+        return value;
+    }
 }
